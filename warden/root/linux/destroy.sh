ad1696ecd93304950a179ba5ef28664a154edc61
wangxiangyu <wangxiangyu@baidu.com>
2014-06-05 11:39:26 +0800

    mount path in dead container

--- warden/root/linux/destroy.sh
+++ warden/root/linux/destroy.sh
@@ -19,6 +19,12 @@ then
   exit 0
 fi
 
+function mount_path_in_dead_container
+{
+  [[-d $backup_path/$id/tmp/rootfs/$1 ]] || mkdir -p $backup_path/$id/tmp/rootfs/$1
+  mount -n --bind $rootfs_path/$1 $backup_path/$id/tmp/rootfs/$1
+  mount -n --bind -o remount,ro $rootfs_path/$1 $backup_path/$id/tmp/rootfs/$1
+}
 if [ -d $target ]
 then
   if [ -f $target/destroy.sh ]
@@ -28,7 +34,15 @@ then
   source $target/etc/config
   if [[ $backup == "true" ]]
   then
-     [[ -d $backup_path ]] || mkdir -p $backup_path && mv $target $backup_path
+    [[ -d $backup_path ]] || mkdir -p $backup_path && mv $target $backup_path
+    if [[ -d $backup_path ]]
+    then
+      mount_path_in_dead_container bin 
+      mount_path_in_dead_container lib
+      mount_path_in_dead_container lib64
+      mount_path_in_dead_container sbin
+      mount_path_in_dead_container usr
+    fi
   else
     rm -rf $target
   fi
